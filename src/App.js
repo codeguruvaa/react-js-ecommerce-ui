3de4b7e0d2668bae9c54e37ca70d3b251f756d94
codeguruvaa <mohanvej@gmail.com>
2022-06-17 10:41:27 +0530

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
+import React from 'react';
+import { Layout } from 'antd';
+import 'antd/dist/antd.css';
 import './App.css';
+import { Typography,Avatar } from 'antd';
 
-function App() {
+const { Title } = Typography;
+const { Header, Footer, Sider, Content } = Layout;
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      
+      <Layout>
+      <Header className='head' style={{backgroundColor:'gray'}}>
+        <h1>Heading</h1>
+        <Title level={2}>h2. Ant Design</Title>
+        <Avatar size={35} icon="user" style={{float:"right",marginTop:"-115px"}} />
+      </Header>
+
+      <Layout>
+        <Sider style={{backgroundColor:'orange'}}>Sider</Sider>
+        <Content style={{padding:'20px'}}>
+          <p>
+            the quick brown fox jumps over the lazy dog the quick brown fox jumps over the lazy dog
+            the quick brown fox jumps over the lazy dog the quick brown fox jumps over the lazy dog
+            the quick brown fox jumps over the lazy dog the quick brown fox jumps over the lazy dog
+            the quick brown fox jumps over the lazy dog the quick brown fox jumps over the lazy dog
+            the quick brown fox jumps over the lazy dog the quick brown fox jumps over the lazy dog
+            the quick brown fox jumps over the lazy dog the quick brown fox jumps over the lazy dog
+            the quick brown fox jumps over the lazy dog the quick brown fox jumps over the lazy dog
+          </p>
+          <p>
+            the quick brown fox jumps over the lazy dog the quick brown fox jumps over the lazy dog
+            the quick brown fox jumps over the lazy dog the quick brown fox jumps over the lazy dog
+            the quick brown fox jumps over the lazy dog the quick brown fox jumps over the lazy dog
+            the quick brown fox jumps over the lazy dog the quick brown fox jumps over the lazy dog
+            the quick brown fox jumps over the lazy dog the quick brown fox jumps over the lazy dog
+            the quick brown fox jumps over the lazy dog the quick brown fox jumps over the lazy dog
+            the quick brown fox jumps over the lazy dog the quick brown fox jumps over the lazy dog
+          </p>
+        </Content>
+        
+      </Layout>
+      <Footer style={{backgroundColor:'gray'}}>Footer</Footer>
+      
+      
+    </Layout>
+
+
     </div>
   );
-}
+};
 
 export default App;
